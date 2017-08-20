16de20544b9f53d58514e2553c8a85f99e55e605
CoderKevinZhang <iaminwc@163.com>
2017-08-20 10:52:09 -0700

    add comment lines

--- sass+compass/solor-eclipse-animation/js/index.js
+++ sass+compass/solor-eclipse-animation/js/index.js
@@ -1,10 +1,12 @@
 $(function () {
 
+    // when the website loading complete, run the function fadeOut after 2s
     $(document).ready(function () {
         $('.loading').delay(2000).fadeOut();
     });
 
-    var yellow_panel_animation = function () { // 过场动画
+    // middle animation
+    var yellow_panel_animation = function () {
         $('.yellow-panel').delay(2400).animate({
             bottom: 0
         }, 600, function () {
@@ -24,30 +26,35 @@ $(function () {
         });
     };
 
+    // round title fadeIn after 0.8s
     var round_title_animation = function () {
         $('.round-title').delay(800).fadeIn();
     };
 
+    // round content fadeIn after 0.8s
     var round_content_animation = function () {
         $('.round-content').delay(800).fadeIn();
     };
 
+    // triangle fadeIn after 0.6s
     var triangle_up_animation = function () {
         $('.triangle-up').delay(600).fadeIn();
     };
 
+    // yellow round marginLeft: -200px after 0.5s
     var yellow_round_animation = function () {
         $('.yellow-round').animate({
             marginLeft: '-200px'
         }, 500);
     };
 
+    // black round marginLeft: -180px after 0.5s
     var black_round_animation = function () {
         $('.black-round').delay(200).animate({
             marginLeft: '-180px'
         }, 500);
     };
 
-    yellow_panel_animation(); // 执行过场动画
+    yellow_panel_animation(); // run middle animation
 
 });
