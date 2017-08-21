9791effa79efaf53e9ca6422140c7959caef0846
CoderKevinZhang <iaminwc@163.com>
2017-08-21 14:34:08 -0700

    update images url collection

--- reactJS/my-gallery-project/src/components/Main.js
+++ reactJS/my-gallery-project/src/components/Main.js
@@ -3,13 +3,28 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+// Get the images data from imagesData.json file
+let imagesData = require('../data/imagesData.json');
+
+/* Use self-running function to let imagesData collect every image url*/
+imagesData = (function (imagesDataArray) {
+  for(let i=0, j=imagesDataArray.length; i<j; i++){
+    let singleImageData = imagesDataArray[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imagesDataArray[i] = singleImageData;
+  }
+
+  return imagesDataArray;
+})(imagesData);
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
