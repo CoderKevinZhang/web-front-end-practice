5e08ca3afe48af6680cf30071e4fc45a4802a508
CoderKevinZhang <iaminwc@163.com>
2017-08-21 15:07:18 -0700

    build a stage for images

--- reactJS/my-gallery-project/src/components/Main.js
+++ reactJS/my-gallery-project/src/components/Main.js
@@ -24,9 +24,10 @@ imagesData = (function (imagesDataArray) {
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="image-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
