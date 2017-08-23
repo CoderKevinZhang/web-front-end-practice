d666e5a4f9627d2deceb2ae9795e675bba949e0b
CoderKevinZhang <iaminwc@163.com>
2017-08-23 12:45:47 -0700

    update rotation of images

--- reactJS/my-gallery-project/src/components/Main.js
+++ reactJS/my-gallery-project/src/components/Main.js
@@ -11,7 +11,7 @@ let imagesData = require('../data/imagesData.json');
 
 /* Use self-running function to let imagesData collect every image url*/
 let imagesLinks = ((imagesDataArray) => {
-  for(let i=0, j=imagesDataArray.length; i<j; i++){
+  for (let i = 0, j = imagesDataArray.length; i < j; i++) {
     let singleImageData = imagesDataArray[i];
 
     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
@@ -22,13 +22,22 @@ let imagesLinks = ((imagesDataArray) => {
   return imagesDataArray;
 })(imagesData);
 
-class ImgFigure extends React.Component{
-  render(){
+class ImgFigure extends React.Component {
+  render() {
 
     let styleObject = {};
 
-    if(this.props.arrange.pos){
+    if (this.props.arrange.pos) {
       styleObject = this.props.arrange.pos;
+    }
+
+    if(this.props.arrange.rotate){
+
+      let degree = this.props.arrange.rotate;
+
+      (['WebkitT', 'MozT', 'msT', 'OT', 't']).forEach((value) => {
+        styleObject[value + 'ransform'] = 'rotate(' + degree +'deg)';
+      })
     }
 
     return (
@@ -68,61 +77,79 @@ class AppComponent extends React.Component {
     }
   }
 
-  getRandomRange(min, max){
-    return Math.floor(Math.random()*(max - min) + min);
+  /* Get random range from >=min & <(max - min)
+   *
+   * @param min: minimum limit, max: maximum limit
+   */
+
+  getRandomRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  /* Get random rotation degree from -deg to deg
+   *
+   * @param deg: degree of rotation
+   */
+
+  getRandomDegree(deg){
+    return (Math.random() > 0.5 ? '' : '-') + Math.floor(Math.random() * deg);
   }
 
   /* Add images to the center
    *
-   * @param centerIndex the index of the image
+   * @param centerIndex: the index of the image
    */
 
-  reArrange(centerIndex){
+  reArrange(centerIndex) {
     let imgsArrangeArr = this.state.imgsArrangeArr,
-        Constans = this.Contans,
-        centerPos = Constans.centerPos,
-        hPosRange = Constans.hPosRange,
-        vPosRange = Constans.vPosRange,
-        hPosRangeLeftScaleX = hPosRange.leftSecX,
-        hPosRangeRightScaleX = hPosRange.rightSecX,
-        hPosRangeY = hPosRange.y,
-        vPosRangeX = vPosRange.x,
-        vPosRangeTopY = vPosRange.topSecY;
+      Constans = this.Contans,
+      centerPos = Constans.centerPos,
+      hPosRange = Constans.hPosRange,
+      vPosRange = Constans.vPosRange,
+      hPosRangeLeftScaleX = hPosRange.leftSecX,
+      hPosRangeRightScaleX = hPosRange.rightSecX,
+      hPosRangeY = hPosRange.y,
+      vPosRangeX = vPosRange.x,
+      vPosRangeTopY = vPosRange.topSecY;
 
-    // set the position of center image
+    // set the position of center image & rotation degree is not required
     let imgArrayCenter = imgsArrangeArr.splice(centerIndex, 1);
     imgArrayCenter[0].pos = centerPos;
 
-    // set the position of top image
-    let topImgNum = Math.floor(Math.random()*2), // either one or no img on the top area
-        topImgIndex = Math.floor(Math.random()*(imgsArrangeArr.length - topImgNum)),
-        topImgArray = imgsArrangeArr.splice(topImgIndex, topImgNum);
+    // set the position of top image & rotation degree is required
+    let topImgNum = Math.floor(Math.random() * 2), // either one or no img on the top area
+      topImgIndex = Math.floor(Math.random() * (imgsArrangeArr.length - topImgNum)),
+      topImgArray = imgsArrangeArr.splice(topImgIndex, topImgNum);
 
     topImgArray.forEach((value, index) => {
       topImgArray[index].pos = {
         left: this.getRandomRange(vPosRangeX[0], vPosRangeX[1]),
         top: this.getRandomRange(vPosRangeTopY[0], vPosRangeTopY[1])
-      }
+      };
+
+      topImgArray[index].rotate = this.getRandomDegree(30);
     });
 
-    // set the position of left & right images
-    for(let i=0, j=imgsArrangeArr.length, k=j/2; i<j; i++){
+    // set the position of left & right images & rotation degree is required
+    for (let i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
       let areaLeftorRight = [];
 
-      if(i<k){
+      if (i < k) {
         areaLeftorRight = hPosRangeLeftScaleX;
-      }else{
+      } else {
         areaLeftorRight = hPosRangeRightScaleX;
       }
 
       imgsArrangeArr[i].pos = {
         left: this.getRandomRange(areaLeftorRight[0], areaLeftorRight[1]),
         top: this.getRandomRange(hPosRangeY[0], hPosRangeY[1])
-      }
+      };
+
+      imgsArrangeArr[i].rotate = this.getRandomDegree(30);
     }
 
     // put topImgArray and imgArrayCenter into imgsArrangeArr
-    if(topImgArray && topImgArray[0]){
+    if (topImgArray && topImgArray[0]) {
       imgsArrangeArr.splice(topImgIndex, 0, topImgArray[0]);
     }
     imgsArrangeArr.splice(centerIndex, 0, imgArrayCenter[0]);
@@ -139,26 +166,26 @@ class AppComponent extends React.Component {
 
     //获取舞台的大小
     let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
-      stageW=stageDOM.scrollWidth,
-      stageH=stageDOM.scrollHeight,
-      halfStageW=Math.floor(stageW/2),
-      halfStageH=Math.floor(stageH/2);
+      stageW = stageDOM.scrollWidth,
+      stageH = stageDOM.scrollHeight,
+      halfStageW = Math.floor(stageW / 2),
+      halfStageH = Math.floor(stageH / 2);
 
     //获取imgFigure的大小
     let imgFigDOM = ReactDOM.findDOMNode(this.refs.image_0),
-      imgFigW=imgFigDOM.scrollWidth,
-      imgFigH=imgFigDOM.scrollHeight,
-      halfImgW=Math.floor(imgFigW/2),
-      halfImgH=Math.floor(imgFigH/2);
+      imgFigW = imgFigDOM.scrollWidth,
+      imgFigH = imgFigDOM.scrollHeight,
+      halfImgW = Math.floor(imgFigW / 2),
+      halfImgH = Math.floor(imgFigH / 2);
 
 
     //计算中心图片的位置
-    this.Contans.centerPos.left=halfStageW-halfImgW;
-    this.Contans.centerPos.top=halfStageH-halfImgH;
+    this.Contans.centerPos.left = halfStageW - halfImgW;
+    this.Contans.centerPos.top = halfStageH - halfImgH;
 
     //计算左右区域的位置
     this.Contans.hPosRange.leftSecX[0] = -halfImgW;
-    this.Contans.hPosRange.leftSecX[1] = halfStageW - halfImgW*3;
+    this.Contans.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
     this.Contans.hPosRange.rightSecX[0] = halfStageW + halfImgW;
     this.Contans.hPosRange.rightSecX[1] = stageW - halfImgW;
     this.Contans.hPosRange.y[0] = -halfImgH;
@@ -167,8 +194,8 @@ class AppComponent extends React.Component {
     //计算上区域的位置
     this.Contans.vPosRange.x[0] = halfStageW - halfImgW;
     this.Contans.vPosRange.x[1] = halfStageW;
-    this.Contans.vPosRange.topSecY[0]= -halfImgH;
-    this.Contans.vPosRange.topSecY[1]=halfStageH - halfImgH * 3;
+    this.Contans.vPosRange.topSecY[0] = -halfImgH;
+    this.Contans.vPosRange.topSecY[1] = halfStageH - halfImgH * 3;
 
     this.reArrange(0);
 
@@ -177,19 +204,21 @@ class AppComponent extends React.Component {
   render() {
 
     let imgFigures = [],
-        controlUnits = [];
+      controlUnits = [];
 
     imagesLinks.forEach((value, index) => {
-      if(!this.state.imgsArrangeArr[index]){ // initiate the state of every image
+      if (!this.state.imgsArrangeArr[index]) { // initiate the state of every image
         this.state.imgsArrangeArr[index] = {
-          pos: {
+          pos: { // initial position of images
             left: 0,
             top: 0
-          }
+          },
+          rotate: 0 // initial rotation degree of images
         }
       }
 
-      imgFigures.push(<ImgFigure data={value} key={index} ref={'image_'+index} arrange={this.state.imgsArrangeArr[index]}/>)
+      imgFigures.push(<ImgFigure data={value} key={index} ref={'image_' + index}
+                                 arrange={this.state.imgsArrangeArr[index]}/>)
     });
 
     return (
@@ -205,7 +234,6 @@ class AppComponent extends React.Component {
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
