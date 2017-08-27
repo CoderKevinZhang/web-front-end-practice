7e9bd57dec16ef9f3e24c1c8e09df392efc44c74
CoderKevinZhang <iaminwc@163.com>
2017-08-27 00:35:29 -0700

    update progress dot moving

--- reactJS/my-music-player-project/src/components/MusicPlayer.js
+++ reactJS/my-music-player-project/src/components/MusicPlayer.js
@@ -3,12 +3,16 @@ import React from 'react';
 class MusicPlayer extends React.Component {
 
   render() {
+
+    // this.props.progress is a percentage value
+    let leftDistance = this.props.progress * 6 + 88;
+
     return (
       <div className="music-player">
         <div className="progress-bar">
           <div className="progress" style={{width: `${this.props.progress}%`}}></div>
         </div>
-        <div className="progress-dot"></div>
+        <div className="progress-dot" style={{left: `${leftDistance}px`}}></div>
         <div id="play"></div>
       </div>
     );
